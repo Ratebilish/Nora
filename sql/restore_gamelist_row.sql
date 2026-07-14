c4c7cd9f100ca2ddfda103b182d481907f610720
Ratebilish <73425214+Ratebilish@users.noreply.github.com>
2026-07-14 15:22:43 +0300

    Delete sql directory

--- sql/restore_gamelist_row.sql
+++
@@ -1,5 +0,0 @@
--- Восстанавливает недостающую строку в gamelist для бота с botid = 2
--- (замените 2 на реальный BOT_ID, если он у вас задан явно в конфигурации)
-
-INSERT INTO gamelist (botid, gamename, ownername, creatorname, map, slotstaken, slotstotal, usernames, totalgames, totalplayers)
-VALUES (2, '', '', '', '', 0, 0, '', 0, 0);
